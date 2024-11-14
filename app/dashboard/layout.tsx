ab022e6c2a9d7fbed257f7c97180ae8c19f88ddd
ebulku <endri.bulku@live.com>
2024-11-14 17:50:22 +0100

    Add Breadcrumbs

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,18 +1,11 @@
-import { AppSidebar } from "@/components/app-sidebar";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-import { Separator } from "@/components/ui/separator";
+import { AppSidebar } from '@/components/app-sidebar';
+import BreadcrumbNav from '@/components/breadcrumb-nav';
+import { Separator } from '@/components/ui/separator';
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
-} from "@/components/ui/sidebar";
+} from '@/components/ui/sidebar';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -23,19 +16,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">
-                    Building Your Application
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
+            <BreadcrumbNav />
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-2">{children}</div>
---
+++ components/breadcrumb-nav.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+  BreadcrumbEllipsis,
+  BreadcrumbPage,
+} from '@/components/ui/breadcrumb';
+import { usePathname } from 'next/navigation';
+import { Fragment } from 'react';
+import { HomeIcon } from 'lucide-react';
+import Link from 'next/link';
+
+const MAX_BREADCRUMB_LENGTH: number = 3;
+
+const BreadcrumbLogic = () => {
+  const paths: string = usePathname();
+  const separator: string = '/';
+  const pathNames: string[] = paths
+    .split(separator)
+    .filter((path) => path !== '');
+
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink asChild>
+            <Link href="/">
+              <HomeIcon className="h-4 w-4" />
+            </Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        {pathNames.length > MAX_BREADCRUMB_LENGTH && <BreadcrumbEllipsis />}
+        {pathNames.slice(-MAX_BREADCRUMB_LENGTH).map((link, index) => {
+          const href: string = `/${pathNames
+            .slice(0, -MAX_BREADCRUMB_LENGTH + index + 1)
+            .join('/')}`;
+          const linkName: string = link[0].toUpperCase() + link.slice(1);
+          const isLastPath: boolean = pathNames.length === index + 1;
+
+          return (
+            <Fragment key={index}>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                {!isLastPath ? (
+                  <BreadcrumbLink asChild>
+                    <Link href={href}>{linkName}</Link>
+                  </BreadcrumbLink>
+                ) : (
+                  <BreadcrumbPage>{linkName}</BreadcrumbPage>
+                )}
+              </BreadcrumbItem>
+            </Fragment>
+          );
+        })}
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+};
+
+export default BreadcrumbLogic;
