a0464f853c196f9ee4f416cb21c71eaaf69602a6
ebulku <endri.bulku@live.com>
2024-11-20 20:25:28 +0100

    Loading Button

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,5 @@
 import { Revenue } from '@prisma/client'
-import { clsx, type ClassValue } from 'clsx'
+import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 export const formatCurrency = (amount: number) => {
@@ -108,7 +108,9 @@ export function formatBytes(
   if (bytes === 0) return '0 Byte'
   const i = Math.floor(Math.log(bytes) / Math.log(1024))
   return `${(bytes / Math.pow(1024, i)).toFixed(decimals)} ${
-    sizeType === 'accurate' ? accurateSizes[i] ?? 'Bytes' : sizes[i] ?? 'Bytes'
+    sizeType === 'accurate'
+      ? (accurateSizes[i] ?? 'Bytes')
+      : (sizes[i] ?? 'Bytes')
   }`
 }
 
