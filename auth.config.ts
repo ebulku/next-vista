89b40d7a6313d90b1ba722a6eef8ef3c247e9201
ebulku <endri.bulku@live.com>
2024-12-05 19:35:56 +0100

    Show welcome Screen in demo only

--- auth.config.ts
+++ auth.config.ts
@@ -8,6 +8,16 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard')
+      const isDemo = process.env.ENVIRONMENT === 'demo'
+      const isWelcomePage = nextUrl.pathname === '/'
+
+      if (isWelcomePage) {
+        if (!isDemo) {
+          return Response.redirect(new URL('/dashboard', nextUrl))
+        }
+        return true
+      }
+
       if (isOnDashboard) {
         if (isLoggedIn) return true
         return false // Redirect unauthenticated users to login page
